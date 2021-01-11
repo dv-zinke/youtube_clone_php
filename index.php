3509a94caf89414f2daf9a7807f2852f7b9d6c54
dv-zinke <dv.zinke@gmail.com>
2021-01-11 20:04:34 +0900

    feat: bootstrap jquery 추가

--- index.php
+++ index.php
@@ -6,13 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VideoTube</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <script src="assets/js/commonAction.js"></script>
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-            hello?
+            <button class="navShowHide">Click me!</button>
         </div>
 
         <div id="sideNavContainer">
@@ -20,7 +25,9 @@
         </div>
 
         <div id="mainSectionContainer">
-
+            <div id="mainContentContainer">
+                Hello everyone
+            </div>
         </div>
     </div>
 </body>
