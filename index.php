2e43fc2eee594deb6ea5f72164b4395d1bd3a405
dv-zinke <dv.zinke@gmail.com>
2021-01-14 19:37:39 +0900

    feat: new version

--- index.php
+++ index.php
@@ -1,49 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VideoTube</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<?php require_once("includes/header.php"); ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    <script src="assets/js/commonAction.js"></script>
-</head>
-<body>
-    <div id="pageContainer">
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png">
-            </button>
 
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
-            </a>
+hello everyobe
 
-            <div class="searchBarContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBar" name="term" placeholder="Search...">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png">
-                    </button>
-                </form>
-            </div>
-        </div>
 
-        <div id="sideNavContainer">
-
-        </div>
-
-        <div id="mainSectionContainer">
-            <div id="mainContentContainer">
-                Hello everyone
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
+                
